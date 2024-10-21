c6cc2fe4064082a07b219383d5eadad3eba52605
TanvirHasan23 <144469192+TanvirHasan23@users.noreply.github.com>
2024-10-21 15:24:00 +0600

    Delete Important Syntax directory

--- Important Syntax/If_name_=='__main__'.py
+++
@@ -1,3 +0,0 @@
-# Python interpreter sets 'special variables', one of which is __name__
-# Python will assign the __name__ variable a value of '__main__' if it's
-# the initial module being run
--- Important Syntax/filter.py
+++
@@ -1,4 +0,0 @@
-# filter(function, iterable)
-numbers = [1,2,3,4,5,6,7,8]
-even_number = list(filter(lambda x: x % 2 == 0, numbers))
-print(even_number)
